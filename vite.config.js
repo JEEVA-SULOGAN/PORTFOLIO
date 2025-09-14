44d63f687f1e6dd226de9053ba834ccb24663bee
Jeevas7771 <jeevasulogan181@gmail.com>
2025-09-14 16:02:44 +0800

    Initial commit from local project

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/PORTFOLIO/', // 👈 replace with your repo name
+})
